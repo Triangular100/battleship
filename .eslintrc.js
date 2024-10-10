64988c75e25a1cd18cd65fc6cfa5db729ffd101a
Triangular100 <139834502+Triangular100@users.noreply.github.com>
2024-10-10 15:33:16 -0600

    Add project boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier', 'jest'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/webpack.*.js'],
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# battleship
+# Battleship
+Battleship game to play with your friends! Page created following the Odin Project. Play [here](https://triangular100.github.io/battleship).
